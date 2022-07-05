e0995a8510eeabf479ab69f7e2fe017251f00456
Sergey Marin <marin.sergey@gmail.com>
2022-07-06 00:23:44 +0300

    fix: table restored

--- main.tf
+++ main.tf
@@ -39,3 +39,34 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+
+resource "aws_dynamodb_table" "merkakis_mentions_table" {
+ name = "merkakis-mentions-table"
+ billing_mode = "PROVISIONED"
+ read_capacity= "5"
+ write_capacity= "5"
+ attribute {
+  name = "postId"
+  type = "S"
+ }
+ hash_key = "postId"
+
+ point_in_time_recovery { enabled = true } 
+ server_side_encryption { enabled = false } 
+ lifecycle {ignore_changes = [write_capacity, read_capacity]}
+}
+          
+resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
+   name = "dynamodb_lambda_policy"
+   role = aws_iam_role.iam_for_lambda.id
+   policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+           "Effect" : "Allow",
+           "Action" : ["dynamodb:*"],
+           "Resource" : "${aws_dynamodb_table.merkakis_mentions_table.arn}"
+        }
+      ]
+   })
+}
